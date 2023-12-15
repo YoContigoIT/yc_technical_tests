8bedfa0ba1ef2cc7187287a1d0fbc27b01d0cf59
victorSA99 <victor.sandoval@yocontigo-it.com>
2023-12-15 14:40:33 -0600

    letra repetida

--- 15-12-23/ejercicios.js
+++ 15-12-23/ejercicios.js
@@ -5,7 +5,6 @@ Calcule la suma de todos los números enteros en un rango dado, incluyendo ambos
 
 function sumaDeRango(numInicio, numFinal) {
   // Tu código aquí
-
 }
 
 /*
@@ -22,14 +21,43 @@ function sumaDeRango(numInicio, numFinal) {
   - La función debe retornar un string que contenga únicamente el carácter identificado.
   - Se espera que el código sea claro, legible y eficiente en cuanto a rendimiento.
   - Considerar casos límite y manejar posibles errores o situaciones inesperadas de manera apropiada.
+
+
+  -primero convertir todos las letras recibidas a minusculas
+  -sacar cada una de las letras que contiene la cadena de texto
+  -comparar esas letras para ver cual es la que tiene mayo numero de concidencias
+
+
 */
 
 function caracteresMasFrecuentes(cadena) {
   //Tu código aquí
-  
+
+  let cadenaMin = cadena.toLowerCase();
+  let cadenaAux = [];
+  let letrarepetida = "";
+  let letra = 0;
+  let cont = 1;
+  for (let i = 0; i < cadenaMin.length; i++) {
+    let cont = 0;
+    for (let j = 1; j < cadenaMin.length; j++) {
+      if (cadenaMin[i] == cadenaMin[j]) {
+        cont++;
+        cadenaAux += cont;
+        console.log(cadenaAux);
+        if (letra < cadenaAux) {
+          letra = cadenaAux;
+          letrarepetida = cadenaMin[j];
+        }
+      }
+    }
+  }
+  console.log(letrarepetida);
 }
 
-module.exports = { 
+caracteresMasFrecuentes("hoalaaa");
+
+module.exports = {
   sumaDeRango,
-  caracteresMasFrecuentes
-}
+  caracteresMasFrecuentes,
+};
---
+++ 15-12-23/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+cadenaMin.length
