78f7743be9370283846ddc35998fd23853e9b522
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2023-12-15 14:29:35 -0600

    feat: add exercises 15-12-23 pt2

--- 15-12-23/ejercicios.js
+++ 15-12-23/ejercicios.js
@@ -11,12 +11,13 @@ function sumaDeRango(numInicio, numFinal) {
   return suma;
 }
 
+
+
 /*
   * 4.- Encontrar el Carácter Más Frecuente:
   Desarrolla una función en JavaScript que, dada una cadena de texto, identifique y devuelva el primer carácter que aparece con mayor frecuencia. La función debe considerar todos los caracteres y no distinguir entre mayúsculas y minúsculas. En caso de que varios caracteres tengan la misma frecuencia máxima, la función deberá retornar un string que contenga únicamente el primer carácter encontrado.
 
   * EXTRA OPCIONAL: En caso de que varios caracteres tengan la misma frecuencia máxima, la función deberá retornar un array con todos estos caracteres.
-  
   Consideraciones:
   - La función debe recibir una cadena de texto como parámetro.
   - Debe considerar todos los caracteres, sin distinción entre mayúsculas y minúsculas.
@@ -28,9 +29,30 @@ function sumaDeRango(numInicio, numFinal) {
 */
 
 function caracteresMasFrecuentes(cadena) {
-  //Tu código aquí
-  
+  var contador = {};
+  for (var i = 0; i < cadena.length; i++) {
+    var char = cadena[i];
+
+    if (contador[char]) {
+      contador[char]++;
+    } else {
+      contador[char] = 1;
+    }
+  }
+
+  var conteoMax = 0;
+  var elMasRepetido = "";
+
+  for (var char in contador) {
+    if (contador[char] > conteoMax) {
+      conteoMax = contador[char];
+      elMasRepetido = char;
+    }
+  }
+
+  return elMasRepetido;
 }
+
 
 module.exports = { 
   sumaDeRango,
