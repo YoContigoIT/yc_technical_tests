ffc19a6c826a0099f7131974c68d26196e45e43c
Arturo Bustamante <arturo.bustamante@yocontigo-it.com>
2023-12-22 17:37:22 -0600

    22 de diciembre

--- 22-12-23/ejercicios-extras.js
+++ 22-12-23/ejercicios-extras.js
@@ -1,14 +1,18 @@
-const sumarArray = (arr) => arr
+const sumarArray = (arr) => { return arr.reduce((acc, val) => acc + val) }
 
-const multiplicarArray = (arr) => arr
+const multiplicarArray = (arr) => { return arr.reduce((acc, val) => acc * val) }
 
-const usarForEach = (arr) => {}
+const usarForEach = (arr) => {
+    var otroArr = [];
+    arr.forEach((x) => { otroArr.push(x*2) })
+    return otroArr;
+}
 
-const usarMap = (arr) => arr
+const usarMap = (arr) => { return arr.map((x) => x*3 )}
 
-const usarFilter = (arr) => arr
+const usarFilter = (arr) => { return  arr.filter((x) => x % 2 === 0)}
 
-const usarFind = (arr, valor) => arr
+const usarFind = (arr, valor) => { return arr.find((x) => x === valor)}
 
 module.exports = {
     sumarArray,
