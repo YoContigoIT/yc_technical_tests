eb65f4f54abe1f4898ad341722db68b5ac5acb1b
bacallaV <valdohalo3@hotmail.com>
2023-12-22 15:40:44 -0600

    feat (22-12-23): extra excercise

--- 22-12-23/ejercicios-extras.js
+++ 22-12-23/ejercicios-extras.js
@@ -1,14 +1,32 @@
-const sumarArray = (arr) => arr
+const sumarArray = (arr) => arr.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    0,
+);
 
-const multiplicarArray = (arr) => arr
+const multiplicarArray = (arr) => arr.reduce(
+    (previousValue, currentValue) => previousValue * currentValue,
+    1,
+);
 
-const usarForEach = (arr) => {}
+const usarForEach = (arr) => {
+    newArr = new Array();
 
-const usarMap = (arr) => arr
+    arr.forEach(
+        element => ( newArr.push(element * multiplier) ),
+        multiplier = 2,
+    );
 
-const usarFilter = (arr) => arr
+    return newArr;
+}
 
-const usarFind = (arr, valor) => arr
+const usarMap = (arr) => arr.map(
+    element => ( element += initialValue += 2 ),
+    initialValue = 0,
+);
+
+const usarFilter = (arr) => arr.filter(element => (element % 2 === 0));
+
+const usarFind = (arr, valor) => arr.find(element => element === valor);
 
 module.exports = {
     sumarArray,
