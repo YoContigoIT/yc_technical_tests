3bae63d07c5c247b4101f20649b582f2b6475fc2
Antonio Tach Viña <antach.vihe@gmail.com>
2023-12-22 14:59:20 -0600

    Ejercicios TACHI 22-12-23

--- 22-12-23/ejercicios-extras.js
+++ 22-12-23/ejercicios-extras.js
@@ -1,14 +1,37 @@
-const sumarArray = (arr) => arr
+const sumarArray = (arr) => {
+    const suma = arr.reduce((acumulador, value) => acumulador + value, 0);
+    return suma;
+}
 
-const multiplicarArray = (arr) => arr
+const multiplicarArray = (arr) => {
+    const multiplicar = arr.reduce((acumulador, value) => acumulador * value, 1);
+    return multiplicar
+}
 
-const usarForEach = (arr) => {}
+const usarForEach = (arr) => {
+    let newArray = [];
+    arr.forEach(element => {
+        newArray.push(element * 2);
+    });
+    return newArray;
+}
 
-const usarMap = (arr) => arr
+const usarMap = (arr) => {
+    const newMap = arr.map(element => {
+        return element * 3;
+    });
+    return newMap;
+}
 
-const usarFilter = (arr) => arr
+const usarFilter = (arr) => {
+    const filter = arr.filter(element => element % 2 === 0);
+    return filter;
+}
 
-const usarFind = (arr, valor) => arr
+const usarFind = (arr, valor) => {
+    const values = arr.find(element => element === valor);
+    return values;
+}
 
 module.exports = {
     sumarArray,
