06cfeee8fb7be8e524f425ab611ab34e15c2e18e
Arturo Bustamante <arturo.bustamante@yocontigo-it.com>
2024-01-12 17:01:56 -0600

    ejercicio resuelto 12 de enero

--- packages/12-01-24/WebServer-Node/actividad/ejercicios/server.js
+++ packages/12-01-24/WebServer-Node/actividad/ejercicios/server.js
@@ -2,9 +2,41 @@ var fs = require("fs");
 var http = require("http");
 /* ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️ */
 /* AQUÍ DEBAJO PUEDES ESCRIBIR LA CONSTANTE DEL PUERTO */
-const PORT = null;
+const PORT = 3001;
+
+const server = http.createServer( function(req, res){
+  console.log(`Server raised in port ${PORT}`);
+
+	if(req.url === '/api'){
+    fs.readFile(__dirname + '/utils/dogsData.json', (err, data) => {
+      if(err){
+        res.writeHead(404, { 'Content-Type': 'text/plain' })
+        res.end('json not found');
+      }
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end(data);
+    });
+    return;
+	} 
+  else if(req.url === '/allDogs') {
+    fs.readFile(__dirname + '/utils/allDogs.html', 'utf8', (err, data) => {
+      if(err){
+        res.writeHead(404, { 'Content-Type': 'text/plain' })
+        res.end('html not found');
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.end(data);
+    });
+    return;
+  }
+
+  res.writeHead(404, { 'Content-Type': 'text/plain' })
+  res.end('Route not found');
+
+}).listen(PORT, '127.0.0.1');
 
 /* ⚠️ LA LÍNEA SIGUIENTE TIENE QUE QUEDAR COMO ESTÁ PARA PODER EXPORTAR EL SERVIDOR ⚠️ */
 module.exports =
   /* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
-  null;
+  server;
+
