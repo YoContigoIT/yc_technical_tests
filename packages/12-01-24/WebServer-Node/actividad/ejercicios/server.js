fc14aaa1183e5d7ea1faafc8c3473662f2218359
Kim Ramirez <kim.rc.1066@gmail.com>
2024-01-17 00:08:40 -0600

    feat: running server added

--- packages/12-01-24/WebServer-Node/actividad/ejercicios/server.js
+++ packages/12-01-24/WebServer-Node/actividad/ejercicios/server.js
@@ -2,7 +2,16 @@ var fs = require("fs");
 var http = require("http");
 /* ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️ */
 /* AQUÍ DEBAJO PUEDES ESCRIBIR LA CONSTANTE DEL PUERTO */
-const PORT = null;
+const PORT = 3001;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, World!\n");
+})
+
+server.listen( PORT, () => {
+  console.log(`Server raised in port ${PORT}`);
+}
+)
 
 /* ⚠️ LA LÍNEA SIGUIENTE TIENE QUE QUEDAR COMO ESTÁ PARA PODER EXPORTAR EL SERVIDOR ⚠️ */
 module.exports =
