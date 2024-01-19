83b71fcf2107c99c4ac4b20fc306168e632025eb
DanielCarrillo16 <108699035+DanielCarrillo16@users.noreply.github.com>
2024-01-19 11:43:51 -0600

    Actividad 12-01-2024

--- packages/12-01-24/WebServer-Node/actividad/ejercicios/server.js
+++ packages/12-01-24/WebServer-Node/actividad/ejercicios/server.js
@@ -2,9 +2,49 @@ var fs = require("fs");
 var http = require("http");
 /* ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️ */
 /* AQUÍ DEBAJO PUEDES ESCRIBIR LA CONSTANTE DEL PUERTO */
-const PORT = null;
+const PORT = 3001;
 
 /* ⚠️ LA LÍNEA SIGUIENTE TIENE QUE QUEDAR COMO ESTÁ PARA PODER EXPORTAR EL SERVIDOR ⚠️ */
-module.exports =
-  /* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
-  null;
+module.exports = http.createServer( function(req, res){
+  //EJERCICIO 1
+  if( req.url === '/'){
+    console.log(`Server raised in port ${PORT}`);
+  }
+  //EJERCICIO 2
+  else if( req.url === '/api'){
+		res.writeHead(200, { 'Content-Type':'application/json' })
+		fs.readFile(__dirname + '/utils/dogsData.json', 'utf-8', function(err, data){
+      if(err){
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('File not found');
+      }
+      else {
+        var jsonData = JSON.parse(data);
+        res.end(JSON.stringify(jsonData));
+      }
+    });
+    return;
+	}
+
+  //EJERCICIO 3
+  else if( req.url === '/allDogs'){
+		res.writeHead(200, { 'Content-Type':'text/html' })
+		fs.readFile(__dirname + '/utils/allDogs.html', 'utf-8', function(err, data){
+      if(err){
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('HTML not found');
+      }
+      else {
+        res.end(data);
+      }
+    });
+    return;
+	}
+
+  // EJERCICIO 4
+  // EXCEPCIÓN DE RUTAS
+  else{
+		res.writeHead(404); 
+		res.end(); 
+	}
+}).listen(PORT, '127.0.0.1');
