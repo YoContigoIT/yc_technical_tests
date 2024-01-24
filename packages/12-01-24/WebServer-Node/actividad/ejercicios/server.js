d6ede6857a134b0dfec620d8ada7221aa2363cec
victorSA99 <victor.sandoval@yocontigo-it.com>
2024-01-24 11:31:22 -0600

    agregando servidor

--- packages/12-01-24/WebServer-Node/actividad/ejercicios/server.js
+++ packages/12-01-24/WebServer-Node/actividad/ejercicios/server.js
@@ -2,9 +2,19 @@ var fs = require("fs");
 var http = require("http");
 /* ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️ */
 /* AQUÍ DEBAJO PUEDES ESCRIBIR LA CONSTANTE DEL PUERTO */
-const PORT = null;
+const PORT = 4000;
 
 /* ⚠️ LA LÍNEA SIGUIENTE TIENE QUE QUEDAR COMO ESTÁ PARA PODER EXPORTAR EL SERVIDOR ⚠️ */
-module.exports =
-  /* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
-  null;
+module.exports = { servidor };
+/* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
+function servidor() {
+  const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+      res.writeHead(200);
+    }
+  });
+
+  server.listen(PORT, () => {
+    console.log("clase de yo contigo");
+  });
+}
---
+++ packages/12-01-24/WebServer-Node/ejemploServer.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  // if(req.url=== '/'){
+  //     res.writeHead(200,)
+  // }
+});
+
+server.listen(8000, () => {
+  console.log("clase de yo contigo");
+});
