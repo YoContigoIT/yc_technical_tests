4614240045a818ebb0b97f7606bb9471bfdf6019
bacallaV <valdohalo3@hotmail.com>
2024-01-26 16:41:21 -0600

    feat(26-01-23): :sparkles: excercise completed

--- packages/26-01-24/demo/server/src/index.js
+++ packages/26-01-24/demo/server/src/index.js
@@ -2,6 +2,9 @@ const crypto = require('crypto')
 const fs = require("fs");
 const express = require("express");
 
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const USERS_FILE = "./users.json";
 
 const app = express();
@@ -13,25 +16,72 @@ if (!fs.existsSync(USERS_FILE)) {
     fs.writeFileSync(USERS_FILE, JSON.stringify([]));
 }
 
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const newUser = req.body;
-  fs.readFile(USERS_FILE, (err, data) => {
-    if (err) {
-      res.status(500).send("Error al leer el archivo");
-      return;
-    }
-    let users = JSON.parse(data);
+  // Crypting password
+  newUser.password = await bcrypt.hash(newUser.password, 10);
 
+  // Reading file
+  fs.readFile(USERS_FILE, (err, data) => {
+    if (err) return res.status(500).send("Error al leer el archivo");
+
+    let users = JSON.parse(data);
     users.push({ id: crypto.randomUUID(), ...newUser });
-    
+    // Writing data
     fs.writeFile(USERS_FILE, JSON.stringify(users, null, 2), (err) => {
-      if (err) {
-        res.status(500).send("Error al guardar el usuario");
-      } else {
-        res.status(200).send("Usuario creado exitosamente");
-      }
+      if (err) return res.status(500).send("Error al guardar el usuario");
+
+      return res.status(200).send("Usuario creado exitosamente");
     });
   });
+});
+
+app.post("/login", async(req, res) => {
+  const user = req.body;
+
+  // Reading file
+  await fs.readFile(USERS_FILE, async(err, data) => {
+    if (err) return res.status(500).send("Error al leer el archivo");
+
+    // Getting user from db json file
+    let users = JSON.parse(data);
+
+    const existingUser = users.find(dbUser => dbUser.username === user.username);
+    if (!existingUser) return res.status(404).send("Credenciales inválidas");
+
+    // Checking password coincidence 
+    const isValid = await bcrypt.compare(user.password, existingUser.password);
+    if(!isValid) return res.status(404).send('Credenciales inválidas');
+
+    return res.status(200).json({
+      token: jwt.sign(
+        { user: user.id },
+        'my-key',
+        { expiresIn: '1h' },
+      ),
+    });
+  });
+});
+
+app.get("/posts", Auth, async (req, res) => {
+  return res.status(200).json([]);
+});
+
+/* Middlewares */
+function Auth (req, res, next) {
+	const token = req.headers.authorization?.split(' ')[1];
+	if(!token) return res.status(401).json('Sin autorización');
+
+	const jwtPayload = jwt.verify(token, 'my-key', (err, decoded) => null);
+	if(!jwtPayload) return res.status(403).json('Token inválido');
+
+	next();
+}
+
+/* Using middleware */
+app.get('/posts', Auth, (req, res) => {
+	// Some code here...
+  return res.status(200).json([]);
 });
 
 const PORT = 3001;
