0a2b2fe0955758618e3d3ef7e49c572004acb9f8
bacallaV <valdohalo3@hotmail.com>
2024-01-26 14:47:21 -0600

    feat(29-12-23): :sparkles: excercises solved

--- packages/29-12-23/ejercicios.js
+++ packages/29-12-23/ejercicios.js
@@ -4,17 +4,20 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  return nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase();
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  cb(n1, n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +25,16 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  cb(
+    numeros.reduce((prev, current, currentIndex) => prev + current)
+  );
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(value => cb(value));
 }
 
 function map(array, cb) {
@@ -35,6 +42,7 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  return array.map(value => cb(value));
 }
 
 function filter(array) {
@@ -42,6 +50,7 @@ function filter(array) {
   // ['abajo', 'pera', 'escalera', 'alerta', 'indice', 'azteca', 'arbol', 'buzo']
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  return array.filter(value => value.charAt(0) === 'a');
 }
 
 /*
